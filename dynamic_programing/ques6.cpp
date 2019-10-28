7fdfb346a80253bccbadbcc64558bc772e9d665f
KokoFan16 <koko14fan@gmail.com>
2019-10-28 11:05:23 -0500

    Integer Break

--- dynamic_programing/ques6.cpp
+++ dynamic_programing/ques6.cpp
@@ -2,6 +2,15 @@
 #include <algorithm>
 #include <vector>
 
+/*
+You are given a positive integer n, break it into the sum of at least two positive integers 
+and maximize the product of those integers. Return the maximum product you can get.
+
+Example Input: 10, Output: 36, Explanation: 10 = 3 3 4 = 36.
+
+What is your answer for n = 82.
+*/
+
 
 int integerBreak(int n)
 {
