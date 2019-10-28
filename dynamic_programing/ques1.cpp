fa1aad38307692bcbc72988f2fe4f85f2139ef7d
KokoFan16 <koko14fan@gmail.com>
2019-10-28 10:55:27 -0500

    FibDp sequences

--- dynamic_programing/ques1.cpp
+++ dynamic_programing/ques1.cpp
@@ -2,11 +2,18 @@
 #include <algorithm>
 #include <vector>
 
+/*
+FibDp sequences: 5, 6, 11, 17, 28 ...
+Take into consideration the numbers in the FibDP series whose values are not more than 50000000000.
+What is the sum of the odd-valued terms?
+*/
+
 
 long long int fib()
 {
     std::vector<long long int> nums;
     long long int f = 0;
+    // First and second items
     nums.push_back(5);
     nums.push_back(6);
     
@@ -15,19 +22,16 @@ long long int fib()
     while (f < 50000000000) {
 
         long long int n = nums.size();
+        // FibDP series
         f = nums[n-1] + nums[n-2];
-        
         if (f > 50000000000)
             continue;
-        
         nums.push_back(f);
-        std::cout << f << std::endl;
         if(f%2 == 1)
         {
             sum += f;
         }
     }
-    
     return sum;
 }
 
