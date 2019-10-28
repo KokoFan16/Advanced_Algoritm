e24d6bdf4d262708d03cce15a96f4288c0e19be4
KokoFan16 <koko14fan@gmail.com>
2019-10-28 10:58:51 -0500

    Unique paths

--- dynamic_programing/ques2.cpp
+++ dynamic_programing/ques2.cpp
@@ -2,11 +2,20 @@
 #include <algorithm>
 #include <vector>
 
+/*
+Starting in the top left corner of m * n grid, and only being able to move to the right 
+and down to the bottom right corner.
+
+For example, a 2 × 2 grid, there are exactly 6 routes. 
+
+How many such routes are there through a 21 × 21 grid?
+*/
 
 long long int findRoutes(int m, int n) {
     std::vector<std::vector<long long int>> routes(m+1, std::vector<long long int>(n+1, 1));
     for (int i = 1; i < m+1; i++) {
         for (int j = 1; j < n+1; j++) {
+            // Each grid can be divided into left and up
             routes[i][j] = routes[i - 1][j] + routes[i][j - 1];
         }
     }
